0a55b7937ee1c5442e7b47874d18a47089de8617
Aman Garg <43947335+aman-garg0001@users.noreply.github.com>
2021-02-10 19:35:59 +0530

    TCP Server interacting with the TCP Client

---
+++ Chat/chat_server.c
@@ -0,0 +1,56 @@
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#define PORT 1024 
+#define SIZE 4096
+
+int main(int argc, char const *argv[]) 
+{ 
+	int server_fd, new_socket, valread; 
+	struct sockaddr_in address; 
+	int opt = 1; 
+	int addrlen = sizeof(address); 
+	char buffer[SIZE]; 
+	
+	// Creating socket file descriptor 
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+	{ 
+		perror("socket failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	address.sin_family = AF_INET; 
+	address.sin_addr.s_addr = INADDR_ANY; 
+	address.sin_port = htons( PORT ); 
+	
+	// Forcefully attaching socket to the port 8080 
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) 
+	{ 
+		perror("bind failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	if (listen(server_fd, 10) < 0) 
+	{ 
+		perror("listen"); 
+		exit(EXIT_FAILURE); 
+	} 
+	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,(socklen_t*)&addrlen))<0) 
+	{ 
+		perror("accept"); 
+		exit(EXIT_FAILURE); 
+	} 
+	while(1)
+	{
+		read(new_socket, buffer, SIZE);
+		printf("CLIENT: %s\n", buffer);
+		if(strcmp(buffer, "bye") == 0)
+			break;
+		printf("SERVER: ");
+		gets(buffer);
+		send(new_socket, buffer, SIZE, 0);
+	}
+	close(new_socket);
+	return 0; 
+} 
